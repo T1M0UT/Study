2e98730c4b566005558a50a736146795446cbb36
T1M0UT <46775934+T1M0UT@users.noreply.github.com>
2020-01-29 15:38:17 +0200

    Update Program.cs

--- Test/Program.cs
+++ Test/Program.cs
@@ -1,55 +1,32 @@
 using System;
-using System.Linq;
-using System.IO;
-using System.Text;
-using System.Collections;
 using System.Collections.Generic;
 namespace Solv
-{ 
-class Solution
 {
-    static void Main(string[] args)
+    class Solution
     {
-        int y=0;
-        Tree tree=new Tree();
-        tree.Add(0);
-        int N = int.Parse(Console.ReadLine());
-        for (int i = 0; i < N; i++)
+        static void Main(string[] args)
         {
-            string telephone = Console.ReadLine();
-            foreach(char x in telephone)
+            int y = 0;
+            Node Root = new Node(0);
+            int N = int.Parse(Console.ReadLine());
+            for (int i = 0; i < N; i++)
             {
-                tree.Add(x);
-                y=tree.Count();
-                Console.WriteLine(y);
+                Node current = Root;
+                string telephone = Console.ReadLine();
+                foreach (char x in telephone)
+                {
+                    int num = x - '0';
+                    current.Add(num);
+                    current = current.Sons[num];
+                }
+                y = Count();
             }
+            Console.WriteLine("OH");
         }
     }
-}
-class Tree
+    class Node
     {
-        private Node Root;
-        
-        public Tree()
-        {
-            Root = null;
-        }
-        public void Add(int newValue)
-        {
-            if (Root == null)
-            {
-                Root = new Node(newValue);
-            }
-            else
-            {
-                Root.Add(newValue);
-            }
-        }
-       public int Count()=>Root.Counter(Root);
-    }
-class Node
-    {
-        private int value;
+        Node[] Sons;
         public Node One { get; private set; }
         public Node Two { get; private set; }
         public Node Three { get; private set; }
@@ -62,7 +39,6 @@ class Node
         public Node O { get; private set; }
         public Node(int value)
         {
-            this.value = value;
             One = null;
             Two = null;
             Three = null;
@@ -76,55 +52,71 @@ class Node
         }
         public void Add(int newValue)
         {
-            switch(newValue){
+            switch (newValue)
+            {
                 case 1:
-                    if (One == null) {One = new Node(newValue);};                    
+                    if (One == null) { One = new Node(newValue); };
                     break;
                 case 2:
-                    if (Two == null) {Two = new Node(newValue);}
+                    if (Two == null) { Two = new Node(newValue); }
                     break;
                 case 3:
-                    if (Three == null) {Three = new Node(newValue);}
+                    if (Three == null) { Three = new Node(newValue); }
                     break;
                 case 4:
-                    if (Four == null) {Four = new Node(newValue);}
+                    if (Four == null) { Four = new Node(newValue); }
                     break;
                 case 5:
-                    if (Five == null) {Five = new Node(newValue);}
+                    if (Five == null) { Five = new Node(newValue); }
                     break;
                 case 6:
-                    if (Six==null) {Six = new Node(newValue);}
+                    if (Six == null) { Six = new Node(newValue); }
                     break;
                 case 7:
-                    if (Seven == null) {Seven = new Node(newValue);}
+                    if (Seven == null) { Seven = new Node(newValue); }
                     break;
                 case 8:
-                    if (Eight == null) {Eight = new Node(newValue);}
+                    if (Eight == null) { Eight = new Node(newValue); }
                     break;
                 case 9:
-                    if (Nine == null) {Nine = new Node(newValue);}
+                    if (Nine == null) { Nine = new Node(newValue); }
                     break;
                 case 0:
-                    if (O == null) {O = new Node(newValue);}
+                    if (O == null) { O = new Node(newValue); }
                     break;
                 default:
-                    Console.WriteLine("ERR");
                     break;
             }
         }
-        public int Counter(Node node){
-            int result=0;
-            if(node.One!=null) Counter(node.One()); 
-            if(node.Two!=null) Counter(node.Two()); 
-            if(node.Three!=null) Counter(node.Three()); 
-            if(node.Four!=null) Counter(node.Four()); 
-            if(node.Five!=null) Counter(node.Five()); 
-            if(node.Six!=null) Counter(node.Six()); 
-            if(node.Seven!=null) Counter(node.Seven()); 
-            if(node.Eight!=null) Counter(node.Eight()); 
-            if(node.Nine!=null) Counter(node.Nine()); 
-            if(node.O!=null) Counter(node.O()); 
-            else result++;
+        public List<int> GetList(List<int> answer)
+        {
+            One?.GetList(answer);
+            answer.Add(value);
+            Two?.GetList(answer);
+            Three?.GetList(answer);
+            Four?.GetList(answer);
+            Five?.GetList(answer);
+            Six?.GetList(answer);
+            Seven?.GetList(answer);
+            Eight?.GetList(answer);
+            Nine?.GetList(answer);
+            O?.GetList(answer);
+            return answer;
+        }
+        public override String ToString() => string.Join(", ", GetList(new List<int>()));
+        public int Counter(Node node)
+        {
+            int result = 1;
+            if (node.One != null) result += Counter(node.One);
+            if (node.Two != null) result += Counter(node.Two);
+            if (node.Three != null) result += Counter(node.Three);
+            if (node.Four != null) result += Counter(node.Four);
+            if (node.Five != null) result += Counter(node.Five);
+            if (node.Six != null) result += Counter(node.Six);
+            if (node.Seven != null) result += Counter(node.Seven);
+            if (node.Eight != null) result += Counter(node.Eight);
+            if (node.Nine != null) result += Counter(node.Nine);
+            if (node.O != null) result += Counter(node.O);
             return result;
         }
     }
